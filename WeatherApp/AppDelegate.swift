d78f77c1bf7d723667282fda91a08ab590d0b089
Stefan Lupascu <stefan.lupascu@p3-group.com>
2018-06-22 16:05:58 +0300

    Prepared the view

--- WeatherApp/AppDelegate.swift
+++ WeatherApp/AppDelegate.swift
@@ -17,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let collectionViewController = LocationsViewController(collectionViewLayout: UICollectionViewFlowLayout())
+        window?.rootViewController = UINavigationController(rootViewController: collectionViewController)
+        
         return true
     }
 
