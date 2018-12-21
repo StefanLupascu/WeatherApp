af6dfc6ffff8ab5bb344d7c3c402c0f8d1b8c18a
Stefan Lupascu <lupascu_stefan97@yahoo.com>
2018-12-21 19:06:19 +0200

    Refactor Login Screen

--- WeatherApp/UI/Screens/Login/LoginViewController.swift
+++ WeatherApp/UI/Screens/Login/LoginViewController.swift
@@ -7,20 +7,32 @@
 //
 
 import UIKit
+import FirebaseAuth
+import SnapKit
 
 final class LoginViewController: UIViewController {
     // MARK: - Properties
     
-    private let loginView = LoginView()
+    private let keyboardHeight: CGFloat = 325
+    
+    private let logoImageView = UIImageView()
+    private let userTextfield = UITextField()
+    private let passwordTextfield = UITextField()
+    private let loginButton = UIButton()
+    private let activityIndicator = UIActivityIndicatorView()
     
     // MARK: - Base class overrides
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loginView.delegate = self
-        view = loginView
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisappear(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        setupButtons()
+        setupTextfields()
         setupGesture()
+        setupUI()
     }
 
     // MARK: - Private Functions
@@ -39,31 +51,92 @@ final class LoginViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    // MARK: - Keyboard Handling
-    
-    @objc private func keyboardWillAppear(_ notification: NSNotification) {
-        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue, view.frame.origin.y == 0 else {
-            return
+    @objc private func loginButtonTapped() {
+        guard let username = userTextfield.text,
+            let password = passwordTextfield.text else {
+                return
         }
         
-        view.frame.origin.y -= keyboardSize.height / 2
+        self.showActivityIndicator()
+        Auth.auth().signIn(withEmail: username, password: password) { (user, error) in
+            if error != nil {
+                self.login(validated: false)
+            } else {
+                self.login(validated: true)
+            }
+        }
     }
     
-    @objc private func keyboardWillDisappear(_ notification: NSNotification) {
-        view.frame.origin.y = 0
+    private func showActivityIndicator() {
+        activityIndicator.frame = view.frame
+        activityIndicator.center = view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.style = UIActivityIndicatorView.Style.gray
+        
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
     }
     
-    @objc private func dismissKeyboard(sender: UITapGestureRecognizer!) {
-        view.endEditing(true)
+    private func setupTextfields() {
+        userTextfield.placeholder = "Username"
+        userTextfield.backgroundColor = .white
+        userTextfield.layer.borderColor = UIColor.black.cgColor
+        userTextfield.textAlignment = .center
+        userTextfield.layer.cornerRadius = 10
+        
+        passwordTextfield.placeholder = "Password"
+        passwordTextfield.isSecureTextEntry = true
+        passwordTextfield.backgroundColor = .white
+        passwordTextfield.layer.borderColor = UIColor.black.cgColor
+        passwordTextfield.textAlignment = .center
+        passwordTextfield.layer.cornerRadius = 10
     }
-}
-
-// MARK: - LoginViewDelegate
-
-extension LoginViewController: LoginViewDelegate {
+    
+    private func setupButtons() {
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.backgroundColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+        loginButton.layer.cornerRadius = 10
+        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        logoImageView.image = UIImage(named: "logo")
+        
+        view.addSubview(logoImageView)
+        view.addSubview(userTextfield)
+        view.addSubview(passwordTextfield)
+        view.addSubview(loginButton)
+        
+        logoImageView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(Padding.f75)
+            $0.centerX.equalToSuperview()
+        }
+        
+        userTextfield.snp.makeConstraints {
+            $0.top.equalTo(logoImageView.snp.bottom).offset(Padding.f200)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(Height.h50)
+            $0.width.equalTo(Height.h300)
+        }
+        
+        passwordTextfield.snp.makeConstraints {
+            $0.top.equalTo(userTextfield.snp.bottom).offset(Padding.f5)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(Height.h50)
+            $0.width.equalTo(Height.h300)
+        }
+        
+        loginButton.snp.makeConstraints {
+            $0.top.equalTo(passwordTextfield.snp.bottom).offset(Padding.f20)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(Height.h50)
+            $0.width.equalTo(Height.h300)
+        }
+    }
+    
     func login(validated: Bool) {
         if validated {
-//            let viewModel = CityViewModel()
             let viewModel = LocationsViewModel()
             let vc = LocationsViewController(viewModel: viewModel)
             let navigationController = UINavigationController(rootViewController: vc)
@@ -73,5 +146,31 @@ extension LoginViewController: LoginViewDelegate {
         else {
             presentAlert(message: "Login Failed!")
         }
+    }
+    
+    // MARK: - Keyboard Handling
+    
+    @objc private func keyboardWillAppear(_ notification: NSNotification) {
+        userTextfield.snp.updateConstraints {
+            $0.top.equalTo(logoImageView.snp.bottom).offset(Padding.f220 - keyboardHeight / 2 )
+        }
+        
+        UIView.animate(withDuration: 0.5) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc private func keyboardWillDisappear(_ notification: NSNotification) {
+        userTextfield.snp.updateConstraints {
+            $0.top.equalTo(logoImageView.snp.bottom).offset(Padding.f200)
+        }
+        
+        UIView.animate(withDuration: 0.5) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc private func dismissKeyboard(sender: UITapGestureRecognizer!) {
+        view.endEditing(true)
     }
 }
